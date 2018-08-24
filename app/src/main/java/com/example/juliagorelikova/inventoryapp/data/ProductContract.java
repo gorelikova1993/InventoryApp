31617f45b8484f41fee4889e180a1ecd1c4e0a6a
Yulia Gorelikova <julia.gorelikova@OCRV.RU>
2018-08-24 15:28:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/juliagorelikova/inventoryapp/data/ProductContract.java
@@ -0,0 +1,22 @@
+package com.example.juliagorelikova.inventoryapp.data;
+
+import android.provider.BaseColumns;
+
+public final class ProductContract {
+
+    private ProductContract() {}
+
+    public static final class ProductEntry implements BaseColumns {
+
+        public final static String TABLE_NAME = "products";
+
+        public final static String _ID = BaseColumns._ID;
+
+
+
+
+
+    }
+
+
+}
